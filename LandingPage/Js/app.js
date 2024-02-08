9f737b4101d0ab06b731c381877e560d1095e46d
aleMontoya22 <vladioso2020@gmail.com>
2024-02-07 18:14:48 -0600

    agregue parte de funcion agregar-elemento

--- LandingPage/Js/app.js
+++ LandingPage/Js/app.js
@@ -4,7 +4,7 @@ const alert = document.querySelector(".alert");
 const grocery = document.getElementById("grocery");
 const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
-const listaCompras = document.getElementById('grocery-list');
+const list = document.getElementById('grocery-list');
 const clearBtn = document.querySelector(".clear-btn");
 
 //Edi option
@@ -12,11 +12,26 @@ let editElement;
 let editFlag = false;
 let editID = "";
 
-//Función para agregar un elemento a la lista
-function agregarElemento(){
-    const nuevoItem = inputItem.value;
+//Agregar event listener al boton de agregar
+form.addEventListener('submit', agregarElemento);
 
+//Función para agregar un elemento a la lista
+function agregarElemento(e) {
+    e.preventDefault();
+    const value = grocery.value;
+
+    const id = new Date().getTime().toString();
+    if (value && !editFlag) {
+        console.log("add item to the list");
+    } else if(value&&editFlag){
+        console.log("editing");
+    } else{
+        displayAlert("Please enter value","danger");
+    }
 }
 
-//Agregar event listener al boton de agregar
-bntAgregar.addEventListener('click',agregarElemento);
+//displat alert
+function displayAlert(text,action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+}
